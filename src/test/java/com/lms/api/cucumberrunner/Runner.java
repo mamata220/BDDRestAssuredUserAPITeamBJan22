4c611110c9b4e52cfe67db79ec88d539500b96fd
Mamata <msahoo.us@gmail.com>
2022-01-06 17:09:02 -0600

    Updated Runner Class javadoc comment

--- src/test/java/com/lms/api/cucumberrunner/Runner.java
+++ src/test/java/com/lms/api/cucumberrunner/Runner.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+/**
+ * Main Runner class to run all cucumber feature files
+ *
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features/",
 					 glue = {"com/lms/api/stepdef/user", "com/lms/api/stepdef/skillmap", "com/lms/api/stepdef/skills"},
